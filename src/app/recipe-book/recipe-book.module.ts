fc19077d0ac754f37aea7e7fd11dc4c6f0bc3dcd
Janith Fernando <janithnfernando@gmail.com>
2025-03-20 12:27:25 +0530

    Add main sub compponents for recipe book

--- src/app/recipe-book/recipe-book.module.ts
+++ src/app/recipe-book/recipe-book.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RecipeBookComponent } from './recipe-book.component';
+import { RecipeListComponent } from './recipe-list/recipe-list.component';
+import { RecipeDetailComponent } from './recipe-detail/recipe-detail.component';
+import { RecipeItemComponent } from './recipe-detail/recipe-item/recipe-item.component';
 
 @NgModule({
-  declarations: [RecipeBookComponent],
+  declarations: [RecipeBookComponent, RecipeListComponent, RecipeDetailComponent, RecipeItemComponent],
   imports: [CommonModule],
   exports: [RecipeBookComponent],
 })
